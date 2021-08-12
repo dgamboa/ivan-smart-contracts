f47214279c0e928b80025c034221e1988c8c5056
Daniel Gamboa <danielgamboa10@gmail.com>
2021-08-12 15:54:02 -0500

    Add destroyable contract extension

---
+++ tutorial/destroyable.sol
@@ -0,0 +1,11 @@
+pragma solidity 0.7.5;
+
+import "./ownable.sol";
+
+contract Destroyable is Ownable {
+    
+    function destroy() public onlyOwner {
+        address payable receiver = msg.sender;
+        selfdestruct(receiver);
+    }
+}
