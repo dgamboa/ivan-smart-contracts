03bda200d0eb551b2de2169a7341a7b67877d68e
Daniel Gamboa <danielgamboa10@gmail.com>
2021-08-25 09:22:17 -0500

    Complete token tutorial and start Capper assignment

--- smart_contracts_201/token_proj/contracts/token.sol
+++ smart_contracts_201/token_proj/contracts/token.sol
@@ -1,9 +1,13 @@
+// SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.0;
 
-import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 
-contract MyToken is ERC20 {
-  constructor() ERC20("MyToken", "MTKN") {
-    _mint(msg.sender, 1000);
+contract MyToken is ERC20Capped {
+  constructor() ERC20("MyToken", "MTKN") ERC20Capped(100000) {}
+
+  function mint (uint256 amount) public virtual {
+    _mint(msg.sender, amount);
   }
 }
---
+++ smart_contracts_201/token_proj/migrations/3_capper_migration.js
@@ -0,0 +1,8 @@
+const Capper = artifacts.require("CapperToken");
+
+const TOKEN_NAME = "Capper";
+const TOKEN_SYMBOL = "CPR";
+
+module.exports = function (deployer) {
+  deployer.deploy(Capper, TOKEN_NAME, TOKEN_SYMBOL);
+};
