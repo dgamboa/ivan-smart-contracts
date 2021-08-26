5f2961aebbbf1684e9b2b57746a1050066fb5d64
Daniel Gamboa <danielgamboa10@gmail.com>
2021-08-26 16:57:44 -0500

    Add tests for Dex contract

--- project_201/dex/contracts/Dex.sol
+++ project_201/dex/contracts/Dex.sol
@@ -6,28 +6,59 @@ pragma experimental ABIEncoderV2;
 import "./Wallet.sol";
 
 contract Dex is Wallet {
+    using SafeMath for uint256;
 
-  enum Side {
-    BUY,
-    SELL
-  }
+    enum Side {
+        BUY,
+        SELL
+    }
 
-  struct Order {
-    uint id;
-    address trader;
-    Side side;
-    bytes32 ticker;
-    uint amount;
-    uint price;
-  }
+    struct Order {
+        uint256 id;
+        address trader;
+        Side side;
+        bytes32 ticker;
+        uint256 amount;
+        uint256 price;
+    }
 
-  mapping(bytes32 => mapping(uint => Order[])) orderBook;
+    uint public nextOrderId = 0;
 
-  function getOrderBook(bytes32 ticker, Side side) view public returns(Order[] memory) {
-    return orderBook[ticker][uint(side)];
-  }
+    mapping(bytes32 => mapping(uint256 => Order[])) orderBook;
 
-  function createLimitOrder() public {
+    function getOrderBook(bytes32 ticker, Side side)
+        public
+        view
+        returns (Order[] memory)
+    {
+        return orderBook[ticker][uint256(side)];
+    }
 
-  }
+    function createLimitOrder(
+        Side side,
+        bytes32 ticker,
+        uint256 amount,
+        uint256 price
+    ) public {
+        if (side == Side.BUY) {
+            require(balances[msg.sender]["ETH"] >= amount.mul(price));
+        } else if (side == Side.SELL) {
+            require(balances[msg.sender][ticker] >= amount.mul(price));
+        }
+
+        Order[] storage orders = orderBook[ticker][uint256(side)];
+
+        orders.push(
+          Order(nextOrderId, msg.sender, side, ticker, amount, price)
+        );
+
+        // Bubble Sort:
+        if (side == Side.BUY) {
+
+        } else if (side == Side.SELL) {
+          
+        }
+
+        nextOrderId++;
+    }
 }
