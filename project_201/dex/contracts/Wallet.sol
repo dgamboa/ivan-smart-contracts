014ef4facb8c7db6e183da93bcb407990a37b085
Daniel Gamboa <danielgamboa10@gmail.com>
2021-08-26 08:57:31 -0500

    Add openzep to project

--- project_201/dex/contracts/Wallet.sol
+++ project_201/dex/contracts/Wallet.sol
@@ -3,5 +3,14 @@
 pragma solidity ^0.8.0;
 
 contract Wallet {
-  
+
+  struct Token {
+    bytes32 ticker;
+    address tokenAddress;
+  }
+  mapping(bytes32 => Token) public tokenMapping;
+  bytes32[] public tokenList;
+
+  mapping(address => mapping(bytes32 => uint256)) public balances;
+
 }
