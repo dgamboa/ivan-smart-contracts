c7f9ad7530e05fd94fe48f588dd4063a66ee2a51
Daniel Gamboa <danielgamboa10@gmail.com>
2021-08-27 11:48:15 -0500

    Write bubble sort algorith for order book

--- project_201/dex/contracts/Dex.sol
+++ project_201/dex/contracts/Dex.sol
@@ -22,7 +22,7 @@ contract Dex is Wallet {
         uint256 price;
     }
 
-    uint public nextOrderId = 0;
+    uint256 public nextOrderId = 0;
 
     mapping(bytes32 => mapping(uint256 => Order[])) orderBook;
 
@@ -41,22 +41,42 @@ contract Dex is Wallet {
         uint256 price
     ) public {
         if (side == Side.BUY) {
-            require(balances[msg.sender]["ETH"] >= amount.mul(price));
+            require(
+                balances[msg.sender]["ETH"] >= amount.mul(price),
+                "Seller has insufficient balance"
+            );
         } else if (side == Side.SELL) {
-            require(balances[msg.sender][ticker] >= amount.mul(price));
+            require(
+                balances[msg.sender][ticker] >= amount.mul(price),
+                "Seller has insufficient balance"
+            );
         }
 
         Order[] storage orders = orderBook[ticker][uint256(side)];
 
         orders.push(
-          Order(nextOrderId, msg.sender, side, ticker, amount, price)
+            Order(nextOrderId, msg.sender, side, ticker, amount, price)
         );
 
         // Bubble Sort:
+        // The BUY order book should be ordered on price from highest to lowest starting at index 0
         if (side == Side.BUY) {
-
+            for (uint256 i = orders.length - 1; i > 0; i--) {
+                if (orders[i].price > orders[i - 1].price) {
+                    Order memory orderToMove = orders[i];
+                    orders[i] = orders[i - 1];
+                    orders[i - 1] = orderToMove;
+                }
+            }
+            // The SELL order book should be ordered on price from lowest to highest starting at index 0
         } else if (side == Side.SELL) {
-          
+            for (uint256 i = orders.length - 1; i > 0; i--) {
+                if (orders[i].price < orders[i - 1].price) {
+                    Order memory orderToMove = orders[i];
+                    orders[i] = orders[i - 1];
+                    orders[i - 1] = orderToMove;
+                }
+            }
         }
 
         nextOrderId++;
